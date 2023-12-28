effe97d90e18236ea26852f5292cc1186a068468
Fernando Reyes N <fersland@outlook.es>
2023-12-27 21:03:34 -0500

    DAL-Angular v2
    Con Bootstrap

--- callAPI/src/app/call/call.component.ts
+++ callAPI/src/app/call/call.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, map } from 'rxjs';
 import { ApiService } from '../service/api.service';
+
+/**
+ * COMPONENTES: Es una unidad, reutilizable que encapsula la logica y la representacion
+ * de una parte especifica de la interfaz de usuario
+ * Los componentes son los bloques que construyen las bases que componen una aplicacion Angular
+ * La clase del componentes es una clase TypeScript que contiene la logica del componente.
+ * Esta clase tambien puede interactuar con servicios, realizar operaciones asincronas y manejar eventos.
+ */
 
 @Component({
   selector: 'app-call',
@@ -8,16 +17,19 @@ import { ApiService } from '../service/api.service';
 })
 export class CallComponent implements OnInit {
   data: any[] = [];
+  developerList$!:Observable<any[]>;
 
-  constructor(private apiService: ApiService) {}
+  constructor(private apiService:ApiService) {}
   ngOnInit(): void {
-    this.llenarData();
+    //this.llenarData();
+    this.developerList$ = this.apiService.getLenguageList(); 
   }
 
+  /*
   llenarData(){
     this.apiService.getData().subscribe( data => {
       this.data = data;
       console.log(this.data);
     });
-  }
+  }*/
 }
