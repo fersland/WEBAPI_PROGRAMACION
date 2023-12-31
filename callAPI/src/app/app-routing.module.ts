0e515d73a8890f44fe521c74c2f103f61395557d
Fernando Reyes N <fersland@outlook.es>
2023-12-30 20:26:54 -0500

    DAL-freyes-angular
    Nueva limpieza

--- callAPI/src/app/app-routing.module.ts
+++ callAPI/src/app/app-routing.module.ts
@@ -1,18 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProgrammerComponent } from './programmer/programmer.component';
-import { ListLenguajeComponent } from './lenguajes/list-lenguaje/list-lenguaje.component';
-import { CreateLenguajeComponent } from './lenguajes/create-lenguaje/create-lenguaje.component';
-import { EditLenguajeComponent } from './lenguajes/edit-lenguaje/edit-lenguaje.component';
-import { ViewLanguageComponent } from './lenguajes/view-language/view-language.component';
 
 const routes: Routes = [
-  { path: 'programmer', component: ProgrammerComponent },
-  /*{ path: 'list-lenguaje', component: ListLenguajeComponent },
-  { path: 'create-lenguaje', component: CreateLenguajeComponent },
-  { path: 'edit-lenguaje', component: EditLenguajeComponent },
-  { path: 'view-language/:id', component: ViewLanguageComponent },*/
-  
   { path: 'lenguajes', loadChildren: () => import('./lenguajes/lenguajes.module').then(m => m.LenguajesModule) },
   { path: 'programadores', loadChildren: () => import('./programadores/programadores.module').then(m => m.ProgramadoresModule) }
 ];
